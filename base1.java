cda213bd132cda241b13dcf09907448b12b5c224
MenmoEugene <menmo_o@163.com>
2018-03-15 22:02:43 +0800

    updata

--- base1.java
+++ base1.java
@@ -17,6 +17,14 @@ arm : 生成标准
 活动的四种启动模式：
 		standard  singleTop  singleTask  singleInstance
 
+standard : 在每次启动活动都会创建一个新的活动实例
+
+singleTop ： 不管点击多少次，只要活动处于返回栈，栈顶就只有一个活动实例。
+
+singleTask ：当启动该活动是时，如果发现已经存在则直接使用该实例，并把在这个活动之上的所有活动统统出栈
+
+singleInstance ：指定这个模式的活动会启动一个新的返回栈来管理这个活动，
+它和其他三个模式的不同之处是可以实其他程序和我们的程序可以共享这个活动的实例。
 
 活动的启动模式可以在AndroidManifest.xml中通过给<activity>标签指定 android:launchMode 属性来选择启动模式
 Android 四大组件：
@@ -36,3 +44,4 @@ setContentView(R.layout.cactivity_main);
 
 安装包的生成路径：
 build -- generated -- intermediates -- outputs -- apk -- debug -- xxx.apk 
+
